7c11b516551349773efd45f1d2cea0f3714c4830
Igor <igor@yoast.com>
2020-10-08 09:19:38 +0200

    Add domReady to elementor script

--- js/src/elementor.js
+++ js/src/elementor.js
@@ -1,26 +1,35 @@
 /* global jQuery */
+import domReady from "@wordpress/dom-ready";
 import ElementorEditorData from "./analysis/elementorEditorData";
 import initElementorEditorIntegration from "./initializers/elementor-editor-integration";
 import initEditorStore from "./initializers/editor-store";
 import initScraper from "./initializers/elementor-scraper";
 import initAdminMedia from "./initializers/admin-media";
-import initElementorData from "./watchers/elementor";
+import initElementorData from "./watchers/editorData/elementor";
 
-window.yoast = window.yoast || {};
-window.yoast.initEditorIntegration = initElementorEditorIntegration;
-window.yoast.EditorData = ElementorEditorData;
+domReady( () => {
+	// Initialize the editor store.
+	const store = initEditorStore();
 
-// Initialize the editor store.
-const store = initEditorStore();
+	// Initialize the editor data watcher.
+	initElementorData();
 
-// Initialize the editor data watcher.
-initElementorData();
+	// Initialize the editor data.
+	const editorData = new ElementorEditorData( () => {
+	}, store );
+	editorData.initialize( window.wpseoScriptData.analysis.plugins.replaceVars.replace_vars );
+	window.editorData = editorData;
+
+	// Initialize the scraper.
+	initScraper( jQuery, store, editorData );
+
+	// Initialize the media library for our social settings.
+	initAdminMedia( jQuery );
+} );
 
 // Initialize the editor integration.
-window.yoast.initEditorIntegration( store );
-const editorData = new window.yoast.EditorData( () => {}, store );
-editorData.initialize( window.wpseoScriptData.analysis.plugins.replaceVars.replace_vars );
-window.editorData = editorData;
+initElementorEditorIntegration();
+
 
 // Initialize the scraper.
 initScraper( jQuery, store, editorData );
